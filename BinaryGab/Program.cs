2426242b02b28b6abbca3dfffa78eaa0d6cb94d0
Mehmet Öztürk <59453560+mehmetztrk21@users.noreply.github.com>
2022-04-01 18:55:00 +0300

    merged.

---
+++ BinaryGab/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace BinaryGab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(9));
+        }
+        public static int solution(int N)
+        {
+            string binary = "";
+            while (N > 1)
+            {
+                binary += (N % 2).ToString();
+                N = N / 2;
+            }
+            binary += N.ToString();
+            binary = new String(binary.Reverse().ToArray());
+            string[] split = binary.Split("1");
+            int result = 0;
+            if (split.Length >= 3)
+            {
+                result = split.Max(i => i.Length);
+
+            }
+            Console.WriteLine("Binary : " + binary);
+            return result;
+        }
+    }
+}
